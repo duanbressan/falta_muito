9fcc556355558c5dccfedac0e40e6915e763b766
duanbressan <duan.trabalho@hotmail.com>
2016-04-04 15:17:00 -0300

    Create Project - First Commit

---
+++ app/src/main/java/duan/example/navigationdrawer/MaterialsActivity.java
@@ -0,0 +1,87 @@
+package duan.example.navigationdrawer;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import duan.example.navigationdrawer.adapters.MyPagerAdapter;
+
+
+public class MaterialsActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private TabLayout mTabLayout;
+    private ViewPager mPager;
+    private MyPagerAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_materials);
+        mAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(mToolbar);
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+        mTabLayout.setTabsFromPagerAdapter(mAdapter);
+
+        mTabLayout.setupWithViewPager(mPager);
+        mPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_materials, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class MyFragment extends Fragment {
+        public static final java.lang.String ARG_PAGE = "arg_page";
+
+        public MyFragment() {
+
+        }
+
+        public static MyFragment newInstance(int pageNumber) {
+            MyFragment myFragment = new MyFragment();
+            Bundle arguments = new Bundle();
+            arguments.putInt(ARG_PAGE, pageNumber + 1);
+            myFragment.setArguments(arguments);
+            return myFragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            Bundle arguments = getArguments();
+            int pageNumber = arguments.getInt(ARG_PAGE);
+            TextView myText = new TextView(getActivity());
+            myText.setText(getActivity().getResources().getString(R.string.pagina) + " " + pageNumber);
+            myText.setGravity(Gravity.CENTER);
+            return myText;
+        }
+    }
+}
+
---
+++ app/src/main/java/duan/example/navigationdrawer/adapters/MyPagerAdapter.java
@@ -0,0 +1,31 @@
+package duan.example.navigationdrawer.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import duan.example.navigationdrawer.MaterialsActivity;
+
+
+public class MyPagerAdapter extends FragmentStatePagerAdapter {
+
+    public MyPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        MaterialsActivity.MyFragment myFragment = MaterialsActivity.MyFragment.newInstance(position);
+        return myFragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 10;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "Tab " + (position + 1);
+    }
+}
